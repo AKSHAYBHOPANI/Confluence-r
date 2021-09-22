3699fc3ac7ba9b7fb25cd37e5873ba7a0b9e36fa
Akshay Bhopani <akshaybhopani@gmail.com>
2021-09-22 15:08:09 +0530

    New Tranparent Navbar

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home(theme) {
   useEffect(() => {
 var toggleButton = document.querySelector('.toggle-menu');
 var navbar = document.querySelector('.header');
-navbar.style.display="none"
 // toggleButton.addEventListener('click', function () {
 //   navbar.classList.toggle('toggle');
 // });
