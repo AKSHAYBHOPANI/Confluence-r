281bb058697de8f41fbd3648fdf7c44f2f15821f
Akshay Bhopani <akshaybhopani@gmail.com>
2021-09-27 16:57:30 +0530

    NEW OURWORK LAYOUT

--- pages/our-work.js
+++ pages/our-work.js
@@ -14,37 +14,32 @@ export default function OurWork() {
 
 
 <div className={styles.home}>
-<a href="https://www.cbhogilal.in/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-		<Image layout="fill" src={`/Clients/Cbhogilal.png`}/><div className={styles.middle}>
-    <div className={styles.text} id="home">CBHOGILAL</div>
-  </div></a>
-	<Link href="https://retreeve.club/"><a href="https://retreeve.club/" className={`${styles.box} ${styles.white}`}>
+<Link href="https://retreeve.club/"><a href="https://retreeve.club/" className={`${styles.box} ${styles.white}`}>
 	<Image layout="fill" src={`/Clients/retreeve.png`}/>
 		<div className={styles.middle}>
     <div className={styles.text} id="about">RETREEVE</div>
   </div>
 	</a></Link>
+<a href="https://www.cbhogilal.in/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+		<Image layout="fill" src={`/Clients/Cbhogilal.png`}/><div className={styles.middle}>
+    <div className={styles.text} id="home">CBHOGILAL</div>
+  </div></a>
+<Link href="https://www.panandthedream.com/"><a href="https://www.panandthedream.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+	<Image layout="fill" src={`/Clients/pan.png`}/>
+	<div className={styles.middle}>
+    <div className={styles.text}>PAN & THE DREAMS</div>
+  </div></a></Link>
+  <Link href="https://shop.gehnajewellers.com/"><a href="https://shop.gehnajewellers.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+	<Image layout="fill" src={`/Clients/gehna.png`}/>
+	<div className={styles.middle}>
+    <div className={styles.text}>GEHNA</div>
+  </div></a></Link>
 	<Link href="https://surgems.co.in/"><a href="https://surgems.co.in/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
 	<Image layout="fill" src={`/Clients/Surgems.png`}/>
 	<div className={styles.middle}>
     <div className={styles.text}>SURGEMS</div>
   </div></a></Link>
-<Link href="https://foxandfifth-consulting.com/"><a href="https://foxandfifth-consulting.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-	<Image layout="fill" src={`/Clients/FoxandFifth.png`}/>
-	<div className={styles.middle}>
-    <div className={styles.text}>FOX & FIFTH</div>
-  </div></a></Link>
-<Link href="https://shop.gehnajewellers.com/"><a href="https://shop.gehnajewellers.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-	<Image layout="fill" src={`/Clients/gehna.png`}/>
-	<div className={styles.middle}>
-    <div className={styles.text}>GEHNA</div>
-  </div></a></Link>
-  <Link href="https://beta.confluence-r.com/IELTS%20I%20Can/"><a href="https://beta.confluence-r.com/IELTS%20I%20Can/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-	<Image layout="fill" src={`/Clients/IELTS.png`}/>
-	<div className={styles.middle}>
-    <div className={styles.text}>IELTS</div>
-  </div></a></Link>
-  <Link href="https://dynamatics.com/"><a href="https://dynamatics.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+<Link href="https://dynamatics.com/"><a href="https://dynamatics.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
 	<Image layout="fill" src={`/Clients/dynamatics.png`}/>
 	<div className={styles.middle}>
     <div className={styles.text}>DYNAMATICS</div>
@@ -54,11 +49,17 @@ export default function OurWork() {
 	<div className={styles.middle}>
     <div className={styles.text}>AASTHA</div>
   </div></a></Link>
-  <Link href="https://www.panandthedream.com/"><a href="https://www.panandthedream.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-	<Image layout="fill" src={`/Clients/pan.png`}/>
+<Link href="https://foxandfifth-consulting.com/"><a href="https://foxandfifth-consulting.com/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+	<Image layout="fill" src={`/Clients/FoxandFifth.png`}/>
 	<div className={styles.middle}>
-    <div className={styles.text}>PAN & THE DREAMS</div>
+    <div className={styles.text}>FOX & FIFTH</div>
   </div></a></Link>
+  <Link href="https://beta.confluence-r.com/IELTS%20I%20Can/"><a href="https://beta.confluence-r.com/IELTS%20I%20Can/" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
+	<Image layout="fill" src={`/Clients/IELTS.png`}/>
+	<div className={styles.middle}>
+    <div className={styles.text}>IELTS</div>
+  </div></a></Link>
+  
 </div>
 
 </div>
