4cadca50c8939ebfd4462ca3a8e20cc316186825
Akshay Bhopani <akshaybhopani@gmail.com>
2021-10-12 18:48:10 +0530

    SERP Color fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33bac59cba909e800a33"; }
+/******/ 		__webpack_require__.h = function() { return "4115eeea55a670bf2443"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
